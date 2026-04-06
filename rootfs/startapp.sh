1c97135984daeab971f0ef03c31ac00af33039cd
404oops <me@404oops.com>
2026-04-06 19:14:46 +0200

    Switch to Broadway

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 set -eu
+mkdir -p /opt
+export HOME=/opt
 
-export HOME=/config
+BROADWAY_DISPLAY="${BROADWAY_DISPLAY:-:5}"
+BROADWAY_PORT="${BROADWAY_PORT:-8085}"
+BROADWAY_BIND="${BROADWAY_BIND:-0.0.0.0}"
+
+/usr/bin/broadwayd --address "${BROADWAY_BIND}" --port "${BROADWAY_PORT}" "${BROADWAY_DISPLAY}" &
+BROADWAY_PID="$!"
+
+cleanup() {
+	kill "${BROADWAY_PID}" 2>/dev/null || true
+}
+
+trap cleanup INT TERM EXIT
+
+export GDK_BACKEND=broadway
 exec /usr/bin/virt-manager
