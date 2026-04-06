8f0e68857cd3df149ec489cc48d357dd000c503f
404oops <me@404oops.com>
2026-04-06 19:58:59 +0200

    Force software rendering for the virt-manager OpenGL context

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -2,4 +2,9 @@
 set -eu
 
 export HOME=/config
+# Force software rendering and disable GTK GL context creation in the container client.
+export LIBGL_ALWAYS_SOFTWARE=1
+export GALLIUM_DRIVER=llvmpipe
+export MESA_LOADER_DRIVER_OVERRIDE=llvmpipe
+export GDK_GL=disable
 exec /usr/bin/virt-manager
