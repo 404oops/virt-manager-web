c47574fd5aa60ac676556a0407a2e545e69959e9
404oops <me@404oops.com>
2026-04-06 19:24:41 +0200

    try and fix Broadway

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -7,7 +7,15 @@ BROADWAY_DISPLAY="${BROADWAY_DISPLAY:-:5}"
 BROADWAY_PORT="${BROADWAY_PORT:-8085}"
 BROADWAY_BIND="${BROADWAY_BIND:-0.0.0.0}"
 
-/usr/bin/broadwayd --address "${BROADWAY_BIND}" --port "${BROADWAY_PORT}" "${BROADWAY_DISPLAY}" &
+# Ensure GTK app connects to the exact Broadway display we start.
+export BROADWAY_DISPLAY
+
+# Some distros ship broadwayd variants without --address/--port.
+if /usr/bin/broadwayd --help 2>&1 | grep -q -- '--port'; then
+	/usr/bin/broadwayd --address "${BROADWAY_BIND}" --port "${BROADWAY_PORT}" "${BROADWAY_DISPLAY}" &
+else
+	/usr/bin/broadwayd "${BROADWAY_DISPLAY}" &
+fi
 BROADWAY_PID="$!"
 
 cleanup() {
